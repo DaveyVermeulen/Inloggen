6f5769ae13c7d9eadc85eef4318ee0bbe3b6f064
DaveyVermeulen <daveyvermeulen@edu.mondialcollege.nl>
2016-05-22 16:49:03 +0200

    Add files via upload

--- Auto_Uitklok.py
+++ Auto_Uitklok.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import time
 import pymysql
@@ -6,20 +6,22 @@ import sys
 import decimal
 from datetime import datetime
 
-#connect database
-db = pymysql.connect(host="proxy53.yoics.net", port=32388, user="python", passwd="admin", db="inklokken")
-
-#werkt als cursor
-cur = db.cursor()
-
 def uitKlok():
-    x = 1
+    #connect database
+    db = pymysql.connect(host="proxy50.weaved.com", port=38031, user="python", passwd="admin", db="inklokken")
+
+    #werkt als cursor
+    cur = db.cursor()x = 1
+
+    text = open("beheer.txt", 'r')
+    verkort = beheer.read()
+    
     query = cur.execute("SELECT * FROM `inkloktijd` WHERE id = (%s)", x)
     
     while query:
         leerling = cur.fetchone()
         ln = leerling[1]
-        tijdIn = leerling[2]
+        tijdIn = leerling[3]
         print(tijdIn)
         cur.execute("DELETE FROM `inkloktijd` WHERE `ln` = (%s)", (ln))
         cur.execute("SELECT * FROM `students` WHERE `ln` = (%s)" , (ln))
@@ -57,13 +59,35 @@ def trigger45():
     if secmid == 58500:
         uitKlok()
         sys.exit("Einde lesdag")
+
+def trigger30():
+    if secmid == 32400:
+        uitKlok()
+    if secmid == 34200:
+        uitKlok()
+    if secmid == 36000:
+        uitKlok()
+    if secmid == 38700:
+        uitKlok()
+    if secmid == 40500:
+        uitKlok()
+    if secmid == 42300:
+        uitKlok()
+    if secmid == 45900:
+        uitKlok()
+    if secmid == 47700:
+        uitKlok()
+        sys.exit("Einde lesdag")
         
 
 while True:
-  tijd = datetime.now()
-  uur = tijd.hour
-  minuut = tijd.minute
-  seconde = tijd.second
-  secmid = tijd.hour*3600 + tijd.minute*60 + tijd.second
-  trigger45()
-  time.sleep(1)
+    tijd = datetime.now()
+    uur = tijd.hour
+    minuut = tijd.minute
+    seconde = tijd.second
+    secmid = tijd.hour*3600 + tijd.minute*60 + tijd.second
+    if verkort == "Yes":
+        trigger30()
+    else:
+        trigger45()
+    time.sleep(1)
--- Database_inkloktijd.py
+++ Database_inkloktijd.py
@@ -4,44 +4,58 @@ import decimal
 from datetime import datetime
 
 #connect database
-db = pymysql.connect(host="proxy53.yoics.net", port=32388, user="python", passwd="admin", db="inklokken")
+db = pymysql.connect(host="proxy50.weaved.com", port=38031, user="python", passwd="admin", db="inklokken")
 
 #werkt als cursor
 cur = db.cursor()
 
-def(inklokken):
+def klok():
+    ln = input("leerlingnummer: ")
+
+    nummer = 1
+
+    while ln:
+        if  cur.execute("SELECT `tijd` FROM `inkloktijd` WHERE `ln` = (%s)" , (ln)):
+            tijdIn = cur.fetchone()[0]
+            cur.execute("DELETE FROM `inkloktijd` WHERE `ln` = (%s)", (ln))
+            cur.execute("SELECT `Naam` FROM `students` WHERE `ln` = (%s)" , (ln))
+            Naam = cur.fetchone()[0]
+            cur.execute("INSERT INTO `logs`  (`ln`, `Naam`, `In/uitloggen`) VALUES (%s, %s, 'Uit')", (ln, Naam))
+            cur.execute("SELECT `Studie_uren` FROM `students` WHERE `ln` = (%s)" , (ln))
+            studieUren = cur.fetchone()[0]
+            tijdNu = datetime.now()
+            secmid = tijdNu.hour*3600 + tijdNu.minute*60 + tijdNu.second
+            SU = (secmid - tijdIn)/2700
+            if SU > 2400:
+                totaal = studieUren + 1
+                cur.execute("UPDATE `students` SET `Studie_uren` = (%s) WHERE `ln`= (%s)" %(totaal, ln))
+            else:
+                totaal = round((decimal.Decimal(str(SU)) + studieUren), 1)
+                cur.execute("UPDATE `students` SET `Studie_uren` = (%s) WHERE `ln`= (%s)" %(totaal, ln))
+            db.commit()
+            print("Je bent uitgeklokt,", Naam)
+            print("Je hebt", totaal, "studie-uren")
+            
+        elif cur.execute("SELECT `Naam` FROM `students` WHERE `ln` = (%s)" , (ln)):
+            tijdNu = datetime.now()
+            tijd = tijdNu.hour*3600 + tijdNu.minute*60 + tijdNu.second
+            Naam = cur.fetchone()[0]
+            cur.execute("INSERT INTO `logs`  (`ln`, `Naam`, `In/uitloggen`) VALUES (%s, %s, 'In')", (ln, Naam))
+            cur.execute("INSERT INTO `inkloktijd`  (`id`, `ln`, `Naam`, `tijd`) VALUES (%s, %s, %s, %s)", (nummer, ln, Naam, tijd))
+            db.commit()
+            print("Je bent ingeklokt,", Naam)
+
+        else:
+            if "BEHEER":
+                print("hier komt beheer functie")
+            else:
+                cur.execute("INSERT INTO `logs` (`ln`, `Naam`) VALUES (0, 'Onbekend')")
+                db.commit()
+                print("Sorry, deze leerling komt niet voor in de database")
+            
+        nummer = nummer + 1
         ln = input("leerlingnummer: ")
 
-        nummer = 1
-
-        while ln:
-                if  cur.execute("SELECT `tijd` FROM `inkloktijd` WHERE `ln` = (%s)" , (ln)):
-                    tijdIn = cur.fetchone()[0]
-                    cur.execute("DELETE FROM `inkloktijd` WHERE `ln` = (%s)", (ln))
-                    cur.execute("SELECT `Naam` FROM `students` WHERE `ln` = (%s)" , (ln))
-                    Naam = cur.fetchone()[0]
-                    cur.execute("SELECT `Studie_uren` FROM `students` WHERE `ln` = (%s)" , (ln))
-                    studieUren = cur.fetchone()[0]
-                    tijdNu = datetime.now()
-                    secmid = tijdNu.hour*3600 + tijdNu.minute*60 + tijdNu.second
-                    SU = (secmid - tijdIn)/2700
-                    totaal = round((decimal.Decimal(str(SU)) + studieUren), 1)
-                    cur.execute("UPDATE `students` SET `Studie_uren` = (%s) WHERE `ln`= (%s)" %(totaal, ln))
-                    db.commit()
-                    print("Je bent uitgeklokt,", Naam)
-                    print("Je hebt", totaal, "studie-uren")
-            
-                else:
-                    tijdNu = datetime.now()
-                    tijd = tijdNu.hour*3600 + tijdNu.minute*60 + tijdNu.second
-                    cur.execute("INSERT INTO `inkloktijd`  (`id`, `ln`, `tijd`) VALUES (%s, %s, %s)", (nummer, ln, tijd))
-                    cur.execute("SELECT `Naam` FROM `students` WHERE `ln` = (%s)" , (ln))
-                    Naam = cur.fetchone()[0]
-                    db.commit()
-                    print("Je bent ingeklokt,", Naam)
-                nummer = nummer + 1
-                ln = input("leerlingnummer: ")
-
-db.close() #sluit database
-
-print("Systeem beindigd")
+    db.close() #sluit database
+    print("Systeem beindigd")
+    
