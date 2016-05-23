a9bc06f262748d74acc095a2ef764d858e22907c
pirak3 <joepvanhulst10@gmail.com>
2016-05-23 13:43:35 +0200

    Update GUI_Auto_Uitklok.py

--- GUI_Auto_Uitklok.py
+++ GUI_Auto_Uitklok.py
@@ -7,24 +7,27 @@ import threading
 from datetime import datetime
 
 def beheer():
-    msg = "Inloggen of registreren?"
-    title = "Beheerder  Menu"
-    keuzes= ["Rooster", "Beheerder inloggen","Beheerder registreren"]
-    out = buttonbox(msg,choices=keuzes)
-    print (out)
-    if out == "Rooster":
-        msg= "Welk lesrooster wilt u gebruiken?"
+    msg = "Inloggen of registreren?"#msg die wordt gedisplayed
+    title = "Beheerder  Menu" #title van box
+    keuzes= ["Rooster", "Beheerder inloggen","Beheerder registreren"] #maakt list die easygui naar knoppen omzet
+    out = buttonbox(msg,choices=keuzes) #keuze box werkt zoals een input functie
+    print (out) #debug
+    if out == "Rooster": #vraagt of de input van the box Rooster was
+        msg= "Welk lesrooster wilt u gebruiken?"#msg die wordt gedisplayed
         keuze=["Verkort","Standaard"]  
-        lesrooster=buttonbox(msg,choices=keuze)
-        rooster = open("rooster.txt", 'w')
-        if lesrooster == "Verkort":
+        lesrooster=buttonbox(msg,choices=keuze) #zelfde als bij inloggen
+        rooster = open("rooster.txt", 'w')#opent rooster
+        if lesrooster == "Verkort":#als keuze verkort is run dit
             rooster.write("Verkort")
             rooster.close()
-        if lesrooster == "Standaard":
+        if lesrooster == "Standaard":#als keuze standaard is run dit
             rooster.write("Standaard")
             rooster.close()
+    if out == "Beheerder registeren":# nog niet af maar kom registreren van beheerder
+        msg=msgbox("Vul uw gegevens in AUB")
+        
 
-def rooster():
+def rooster(): #checkt wat de waarde is van rooster en dan runt het bijhorende rooster
     file = open("rooster.txt", 'r')
     rooster = file.read()
     print(rooster)
@@ -36,7 +39,7 @@ def rooster():
         trigger45()
             
     
-def uitKlok():
+def uitKlok():#inloggen bij database en uitloggen van leerlingen na trigger van roosters
     #connect database
     db = pymysql.connect(host="proxy50.weaved.com", port=38031, user="python", passwd="admin", db="inklokken")
 
@@ -44,20 +47,20 @@ def uitKlok():
     cur = db.cursor()
     x = 1
  
-    query = cur.execute("SELECT * FROM `inkloktijd` WHERE id = (%s)", x)
+    query = cur.execute("SELECT * FROM `inkloktijd` WHERE id = (%s)", x) #vraagt eerste line op van table inklok
     
     while query:
-        leerling = cur.fetchone()
+        leerling = cur.fetchone() #convert naar tuple in python
         ln = leerling[1]
         tijdIn = leerling[3]
-        cur.execute("DELETE FROM `inkloktijd` WHERE `ln` = (%s)", (ln))
-        cur.execute("SELECT * FROM `students` WHERE `ln` = (%s)" , (ln))
+        cur.execute("DELETE FROM `inkloktijd` WHERE `ln` = (%s)", (ln)) #verwijdert leerling uit table inklok
+        cur.execute("SELECT * FROM `students` WHERE `ln` = (%s)" , (ln)) #vraagt alle informatie op uit table students
         data = cur.fetchone()
         Naam = data[1]
         studieUren = data[2]
         tijdNu = datetime.now()
-        secmid = tijdNu.hour*3600 + tijdNu.minute*60 + tijdNu.second
-        SU =(secmid - tijdIn)/2700
+        secmid = tijdNu.hour*3600 + tijdNu.minute*60 + tijdNu.second #aantal seconden sinds middernacht
+        SU =(secmid - tijdIn)/2700 #berekent aantal seconden van in tot uitklok
         if SU > 2400:
             totaal = studieUren + 1
             cur.execute("UPDATE `students` SET `Studie_uren` = (%s) WHERE `ln`= (%s)" %(totaal, ln))
@@ -66,13 +69,13 @@ def uitKlok():
             cur.execute("UPDATE `students` SET `Studie_uren` = (%s) WHERE `ln`= (%s)" %(totaal, ln))
         db.commit()
         x = x + 1
-        query = cur.execute("SELECT * FROM `inkloktijd` WHERE id = (%s)", x)
+        query = cur.execute("SELECT * FROM `inkloktijd` WHERE id = (%s)", x) #gaat verder in table inklok, nu bij line x = x + 1
         
 def trigger45():
-    tijd = datetime.now()
-    secmid = tijd.hour*3600 + tijd.minute*60 + tijd.second
+    tijd = datetime.now() # geeft tijd vanaf systeem
+    secmid = tijd.hour*3600 + tijd.minute*60 + tijd.second #seconden vanaf middernacht
     while secmid:
-        if secmid == 33000:
+        if secmid == 33000:#als tijd is einde lesuur, klok dan iedereen uit
             uitKlok()
         if secmid == 36000:
             uitKlok()
@@ -90,12 +93,12 @@ def trigger45():
             uitKlok()
         if secmid == 58500:
             uitKlok()
-            sys.exit("Einde lesdag")
-        time.sleep(1)
+            sys.exit("Einde lesdag")#systeem sluit, niemand kan weer inklokken
+        time.sleep(1)#iedere seconde wordt gecheckt of het al tijd is(einde van het uur)
         tijd = datetime.now()
         secmid = tijd.hour*3600 + tijd.minute*60 + tijd.second
 
-def trigger30():
+def trigger30(): # voor uitleg naar trigger45, nog niet geheel omgeschreven
     tijd = datetime.now()
     secmid = tijd.hour*3600 + tijd.minute*60 + tijd.second
     if secmid == 32400:
@@ -117,16 +120,16 @@ def trigger30():
         sys.exit("Einde lesdag")
 
            
-plaatje = "logo_startpagina.gif"
+plaatje = "logo_startpagina.gif"#plaatje van school logo
 msg = "Welke modus wilt u?"
 keuze = ["Inloggen","Beheer","Afsluiten"]
 antwoord = buttonbox(msg,image= plaatje, choices= keuze ) #Laat venster zien en geeft input terug
 
-print(antwoord)
-if antwoord == "Beheer":
-    beheer()
+print(antwoord)#debug
+if antwoord == "Beheer":#checkt wat het antwoord is
+    beheer()#eerder gedefined
 if antwoord == "Inloggen":
-    rooster()
+    rooster()#eerder gedefined
 if antwoord == "Afsluiten":
     exit("Systeem sluit af")
     
