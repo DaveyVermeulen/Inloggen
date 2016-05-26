dfefc645bb1c83b43c3def434e1bc0d7436e1efa
pirak3 <joepvanhulst10@gmail.com>
2016-05-26 09:54:53 +0200

    Update GUI alleen beheerder werkend.py

--- GUI alleen beheerder werkend.py
+++ GUI alleen beheerder werkend.py
@@ -1,31 +1,37 @@
 from easygui import *
 from time import sleep
-def  beheerder():
-    msg = "Houdt uw pasje voor de scanner"
-    title = "Beheerder inloggen"
-    fieldNames = ["Name","Street Address","City","State","ZipCode"]
-    fieldValues = []  # we start with blanks for the values
-    fieldValues = multenterbox(msg,title, fieldNames)
-
-    # make sure that none of the fields was left blank
-    while 1:
-        if fieldValues == None: break
-        errmsg = ""
-        for i in range(len(fieldNames)):
-            if fieldValues[i].strip() == "":
-                errmsg = errmsg + ('"%s" is a required field.\n\n' % fieldNames[i])
-        if errmsg == "": break
-        fieldValues = multenterbox(errmsg, title, fieldNames, fieldValues)
+def beheerder:
+    msg="Wat wilt u doen?"
+    title="Beheerder"
+    keuze=["Leerling(en) toevoegen","Data bewerken","Rooster veranderen"]
+    wat=buttonbox(msg,title=title,choices=keuze)
+    if wat == "Leerlingen(en) toevoegen":
+        #wachten op pasje
+        if #wachten op pasje == true
+            msg = "Voer de informatie van de leerling in:"
+            title = "Credit Card Application"
+            waardes = ["Naam","Leerlingnummer","Klas",]
+            invoer = []  
+            invoer = multenterbox(msg,title, waardes)
+        while 1:
+            if invoer == None: break
+            errmsg = ""
+            for i in range(len(invoer)):
+                if invoer[i].strip() == "":
+                    errmsg = errmsg + ('"%s" is een verplichte invoer.\n\n' % waardes[i])
+            if errmsg == "": break # no problems found
+            invoer = multenterbox(errmsg, title, waardes, invoer)
+            print "Informatie is:", invoer
+    if wat =="Data bewerken":
+        msg="Wat wilt u bewerken?"
+        title="Data bewerken"
+        keuze=["Pasje(s) bewerken","Leerlingnummer bewerken","Naam bewerken"]
+        
+        
     
-    print ("Reply was:", fieldValues)
-plaatje = "logo_startpagina.gif"
-msg = "Welke modus wilt u?"
-keuze = ["Inloggen","Beheerder","Afsluiten","Test"]
-antwoord = buttonbox(msg,image= plaatje, choices= keuze ) #Laat venster zien en geeft input terug
 
-print(antwoord)
-if antwoord == "Beheerder":
-    beheerder()
+
+if tag =
     
 
 
