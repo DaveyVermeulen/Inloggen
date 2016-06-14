db3bbedb10b018d66a0675a1983cf36b0ab19fc6
hotStuffz <leightonvangellecom@gmail.com>
2016-06-14 09:44:01 +0200

    Update Display_GUI herpe 2.1.py

--- Display_GUI herpe 2.1.py
+++ Display_GUI herpe 2.1.py
@@ -11,9 +11,16 @@ cur = db.cursor()
 class MultiColumnListbox(object):
 
     def __init__(self):
+        self.root = None
+        self.hide = 1
+        self.show = 100
         self.tree = None
         self._setup_widgets()
         self._build_tree()
+        
+    def close(self):
+        self.root.destroy()
+        return
 
     def _setup_widgets(self):
         s = """
@@ -36,8 +43,8 @@ class MultiColumnListbox(object):
         hsb.grid(column=0, row=1, sticky='ew', in_=container)
         container.grid_columnconfigure(0, weight=1)
         container.grid_rowconfigure(0, weight=1)
-            
-
+        self.root.after(self.show*10, self.loop)
+        
     def _build_tree(self):
         for col in display_header:
             self.tree.heading(col, text=col.title(),
@@ -52,14 +59,24 @@ class MultiColumnListbox(object):
                 if self.tree.column(display_header[ix],width=None)<col_w:
                     self.tree.column(display_header[ix], width=col_w)
 
-def sortby(tree, col, descending):
-    data = [(tree.set(child, col), child) \
-        for child in tree.get_children('')]
-    data.sort(reverse=descending)
-    for ix, item in enumerate(data):
-        tree.move(item[1], '', ix)
-    tree.heading(col, command=lambda col=col: sortby(tree, col, \
-        int(not descending)))
+    def sortby(tree, col, descending):
+        data = [(tree.set(child, col), child) \
+            for child in tree.get_children('')]
+        data.sort(reverse=descending)
+        for ix, item in enumerate(data):
+            tree.move(item[1], '', ix)
+        tree.heading(col, command=lambda col=col: sortby(tree, col, \
+            int(not descending)))
+    
+    def loop(self):
+        if self.root:
+            self.root.destroy()
+        time.sleep(self.hide*60)
+        self.(_setup_widgets)
+        self.(_build_tree)
+        self.(sortby)
+        self.root.mainloop()
+        return
 
 root = tk.Tk()
 root.title("THE KRIP")
@@ -68,10 +85,5 @@ display_header = ['Naam', 'Leerlingnummer']
 display_list = info
 listbox = MultiColumnListbox()
 
-def tick():
-    cur.execute("SELECT `Naam`, `Leerlingnummer` FROM `Inkloktijd`")
-    info = cur.fetchall()
-    root.after(1000, tick)
 
-tick()
-root.mainloop()
+MultiColumnListbox().loop()
